9e072b2219fd3f1d3e5f89d8f603f42e4e4e58da
ConnorHoare <connor@tipcigroup.com>
2024-11-06 13:58:16 +0000

    Real time search

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,12 +1,13 @@
 import { defineQuery } from "next-sanity";
 
-export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt desc) {
+export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || category match $search || title match $search || author->name match $search] | order(_createdAt desc) {
     _id, 
     title, 
     slug, 
     _createdAt, 
     author -> {
-      _id, name, image, bio}, 
+      _id, name, image, bio
+    }, 
     views, 
     description, 
     category, 
