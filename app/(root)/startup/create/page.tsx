5091063c3a1dd8e820e8901ecbb282446b558fe5
ConnorHoare <connor@tipcigroup.com>
2024-11-11 17:08:29 +0000

    server actions

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -1,21 +1,21 @@
-import { auth } from "@/auth"
-import StartupForm from "@/components/StartupForm"
-import { redirect } from "next/navigation"
+import StartupForm from "@/components/StartupForm";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
-    const session = await auth();
+  const session = await auth();
 
-    if(!session) redirect('/');
-    return (
-        <>
-            <section className="pink_container !min-h-[230px]">
-                <h1 className="heading">
-                    Submit Your Startup
-                </h1>
-            </section>
-            <StartupForm />
-        </>
-    )
-}
+  if (!session) redirect("/");
+
+  return (
+    <>
+      <section className="pink_container !min-h-[230px]">
+        <h1 className="heading">Submit Your Startup</h1>
+      </section>
+
+      <StartupForm />
+    </>
+  );
+};
 
 export default Page;
